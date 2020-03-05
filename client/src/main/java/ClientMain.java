867f7f29caf07a8b645e52e39a08cba28987eb11
YiSu <strugglm1@gmail.com>
2020-03-05 20:51:12 +0800

    客户端和服务端可以相互完成一次通信

--- client/src/main/java/ClientMain.java
+++ client/src/main/java/ClientMain.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * @description:
@@ -11,9 +13,24 @@ public class ClientMain {
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("127.0.0.1", 8899);
+            //输入流
+            Scanner scanner = new Scanner(socket.getInputStream());
+            //等待服务端建立连接的消息
+            while(true) {
+                if (scanner.hasNext()) {
+                    System.out.println(scanner.next());
+                    break;
+                }
+            }
+            //回复消息
+            System.out.println("客户端： 已经成功跟服务端建立连接，请回复：");
+            //获取输出流，发送消息
+            PrintStream printStream = new PrintStream(socket.getOutputStream());
+            Scanner reply = new Scanner(System.in);
+            printStream.println(reply.nextLine());
+
         } catch (IOException e) {
-
-
+            e.printStackTrace();
         }
 
     }
--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * @description:
@@ -8,12 +10,32 @@ import java.net.Socket;
  * @create: 2020/03/02 22:04
  */
 public class ServerMain {
+
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(8899);
             //等待客户端建立连接
-            Socket accept = serverSocket.accept();
-            System.out.println("与客户端成功建立连接");
+            Socket client = serverSocket.accept();
+            //输出流
+            PrintStream printStream = new PrintStream(client.getOutputStream());
+            //回复建立连接
+            printStream.println("你好，我是服务端，已成功建立连接。");
+            //获取输入流
+            Scanner scanner = new Scanner(client.getInputStream());
+            try {
+                while (true) {
+                    if (scanner.hasNext()) {
+                        System.out.println(scanner.next());
+                        //断开
+                        client.close();
+                        break;
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                client.close();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
